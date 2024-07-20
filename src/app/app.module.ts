23af34941f5e53db41348adef8b36efc72e370ab
Claudio Carigé <ccarige@gmail.com>
2024-07-20 10:58:12 -0300

    [ addition ] : The project's main page was created - Home-Principal
    - This page will be the fixed transition page on the screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule    } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent     } from './app.component';
+import { HomePrincipalComponent } from './infra/pages/home-principal/home-principal.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePrincipalComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
