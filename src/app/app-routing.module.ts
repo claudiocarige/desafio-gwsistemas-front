42b96c137f301dcf618fb1767191d4189219728f
Claudio Carigé <ccarige@gmail.com>
2024-07-20 14:41:36 -0300

    Created a new route for list-customers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePrincipalComponent } from './infra/pages/home-principal/home-principal.component';
+import { ListCustomersComponent } from './core/components/customers/list-customers/list-customers.component';
 
 const routes: Routes = [
+  { path: '', redirectTo:'home', pathMatch:'full'},
   { path:'home', component: HomePrincipalComponent, children:[
+    { path: 'customers', component: ListCustomersComponent}
 
   ]}
 ];
