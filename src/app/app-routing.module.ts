e1cbea5c6857e9cc94d00bd97d30edb00401171b
Claudio Carigé <ccarige@gmail.com>
2024-07-20 11:56:49 -0300

    Create home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePrincipalComponent } from './infra/pages/home-principal/home-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: HomePrincipalComponent, children:[
+
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
