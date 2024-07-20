ca8a87bf75e87cbe10e8508f9292f5701c5b4768
Claudio Carigé <ccarige@gmail.com>
2024-07-20 11:38:06 -0300

    [ feat ] : Install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HomePrincipalComponent } from './infra/pages/home-principal/home-princi
 import { HeaderPrincipalComponent } from './core/components/header-principal/header-principal.component';
 import { FooterPrincipalComponent } from './core/components/footer-principal/footer-principal.component';
 import { SideMenuComponent } from './core/components/side-menu/side-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomePrincipalComponent, HeaderPrincipalComponent, FooterPrincipalComponent, SideMenuComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
